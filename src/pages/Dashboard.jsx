b3e18560e3aa034cf75169b1703fcb2f0b49046e
“Kasun <kasunakalanka4@gmail.com>
2023-08-14 05:14:52 +0530

    upload image to cloudinary

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,45 +2,77 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import BASE_URL from "../config/config";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 const Dashboard = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [pic, setPic] = useState();
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleUpload = async() => {
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append('image', selectedFile);
-
-     const response = await axios.post(`${BASE_URL}/prediction`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-    console.log('response', response.data)
+  const handleUpload = async () => {
+    try {
+      if (selectedFile) {
+        const formData = new FormData();
+        formData.append("image", selectedFile);
+        console.log("form data", formData);
+        const response = await axios.post(`${BASE_URL}/prediction`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        console.log("response", response.data);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
+  const uploadToCloudinary = async () => {
+    const data = new FormData();
+    data.append("file", selectedFile);
+    data.append("upload_preset", "MERNAuthentication");
+    data.append("cloud_name", "dinmf92zr");
+
+    try {
+      const response = await fetch(
+        "https://api.cloudinary.com/v1_1/dinmf92zr/image/upload",
+        {
+          method: "post",
+          body: data,
+        }
+      );
+
+      const cloudinaryData = await response.json();
+      setPic(cloudinaryData.url.toString());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("pic", pic);
+  }, [pic]);
+
   return (
     <>
-    <section className="heading">
-      <h1>Welcome</h1>
-    </section>
-    <section>
-    <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Default file input example</Form.Label>
-        <Form.Control type="file" onChange={handleFileChange}  />
-        <button onClick={handleUpload}>Upload</button>
-      </Form.Group>
-    </section>
-  </>
-  )
- 
-  
+      <section className="heading">
+        <h1>Welcome</h1>
+      </section>
+      <section>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Default file input example</Form.Label>
+          <Form.Control type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Get Predictions</button>
+          <br></br>
+          <br></br>
+          <button onClick={uploadToCloudinary}>Upload</button>
+        </Form.Group>
+      </section>
+    </>
+  );
 };
 
 export default Dashboard;
