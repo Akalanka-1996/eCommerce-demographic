f3365f113288d899a871124d1a2af0a09a261e2e
“Kasun <kasunakalanka4@gmail.com>
2023-08-14 04:26:26 +0530

    send image to backend done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,48 +1,46 @@
 import React from "react";
-import { useEffect} from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import BASE_URL from "../config/config";
+import Form from 'react-bootstrap/Form';
 
 const Dashboard = () => {
-  const navigate = useNavigate();
+  const [selectedFile, setSelectedFile] = useState(null);
 
-//   const { user } = useSelector((state) => state.auth);
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-//   useEffect(() => {
-//     if (!user) {
-//       navigate("/login");
-//     }
-//   }, [user, navigate]);
+  const handleUpload = async() => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('image', selectedFile);
+
+     const response = await axios.post(`${BASE_URL}/prediction`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    console.log('response', response.data)
+    }
+  };
 
   return (
     <>
-      <section className="heading">
-        <h1>Welcome</h1>
-      </section>
-
-      {/* {user?.userRole === "basic" ? (
-        <>
-        <RequestForm />
-        <RequestTable />
-        
-        </>
-      ) : (
-        [user?.userRole === "admin" ? 
-        
-          <>
-          <AdminHeader />
-          <AdminTable />
-          </>
-           : 
-          <>
-           <PostHeader />
-           <PostTable />
-          </>
-         ]
-      )} */}
-
-
-    </>
-  );
+    <section className="heading">
+      <h1>Welcome</h1>
+    </section>
+    <section>
+    <Form.Group controlId="formFile" className="mb-3">
+        <Form.Label>Default file input example</Form.Label>
+        <Form.Control type="file" onChange={handleFileChange}  />
+        <button onClick={handleUpload}>Upload</button>
+      </Form.Group>
+    </section>
+  </>
+  )
+ 
+  
 };
 
 export default Dashboard;
